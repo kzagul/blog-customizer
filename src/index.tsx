9dc724c71d2622b37746989ba401cbfa72bd8790
Загул Кирилл <knzagul@asuproject.ru>
2024-08-23 01:53:45 +0500

    добавлен функционал кастомизации

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,42 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
-import clsx from 'clsx';
-
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
-
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
+import { StrictMode, CSSProperties, useState } from 'react';
+import clsx from 'clsx';
+import { Article } from './components/article/Article';
+import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './constants/articleProps';
+import { useToggle } from 'src/hooks';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [articleState, setArticleState] = useState(defaultArticleState);
+	const [isOpen, actions] = useToggle();
+	const onChangeData = (props: ArticleStateType) => setArticleState(props);
+
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
-			<Article />
+			<ArticleParamsForm
+				onChangeData={onChangeData}
+				actions={actions}
+				isFormOpen={isOpen}
+			/>
+			<Article onClick={() => actions.close()} />
 		</div>
 	);
 };
